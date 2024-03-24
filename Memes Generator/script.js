7fc23930da15c1f777bb81ddd465a4c580643049
Muhammad Aitazaz Ahsan <152264393+aitazazahsan01@users.noreply.github.com>
2024-03-23 23:56:12 -0700

    Add files via upload

---
+++ Memes Generator/script.js
@@ -0,0 +1,21 @@
+const generateButton = document.querySelector('.generate-button');
+const memeImage = document.querySelector('.meme-generator img');
+const memeTitle = document.querySelector('.meme-title');
+const memeAuthor = document.querySelector('.meme-author');
+
+const updateDetails = (url, title ,author)=>{
+    memeImage.setAttribute("src" , url);
+    memeTitle.innerHTML = title;
+    memeAuthor.innerHTML = `Meme by : ${author}`;
+};
+
+const generateMeme = ()=>{
+    fetch("https://meme-api.com/gimme/wholesomememes").then((response) => response.json().then((data) =>{
+                updateDetails(data.url, data.title, data.author);
+
+    }));
+};
+
+generateButton.addEventListener("click", generateMeme);
+
+generateMeme();
